4ecb2c607bb28c8c6e6d87182922b3ecd3a197f4
jessebillard <jessebillard@mac.com>
2018-09-11 10:07:06 -0400

    resolves delete button click not also triggering selectNote method

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,33 +1,38 @@
 import React from 'react'
-import { Segment, Header } from 'semantic-ui-react'
+import { Segment, Button, Icon, Header } from 'semantic-ui-react'
 import { connect } from 'react-redux'
 import { selectNote, deleteNote } from '../actions/index'
-import DeleteButton from './DeleteButton';
 
 class Note extends React.Component {
     
     // console.log(props)
-    handleClick = () => {
+    handleClick = (e) => {
         // console.log("shit fired")
-        this.props.selectNote(this.props.title)
+        // if the target clicked is a button, then don't do the selectNote method
+        console.log(e.target.tagName)
+        if (e.target.tagName !== 'I') {
+            this.props.selectNote(this.props.title)
+        }
     }
 
-    // handleDeleteClick = () => {
-    //     this.props.deleteNote(this.props.id)
+    handleDeleteClick = () => {
+        this.props.deleteNote(this.props.id)
         // console.log(this.props.id)
         // debugger;
-    // }
+    }
 
     render() {
-        // const inlineStyle = {
-        //     button : {
-        //       float: 'right',              
-        //     }
-        //   };
+        const inlineStyle = {
+            button : {
+              float: 'right',              
+            }
+          };
         return (
             <Segment clearing onClick={this.handleClick}>
                 <Header floated='left' as='h6'>{this.props.title}</Header> 
-                <DeleteButton id={this.props.id} />          
+                <Button onClick={this.handleDeleteClick} icon style={inlineStyle.button}>
+                    <Icon name='trash alternate' className="delete"/>                
+                </Button>           
             </Segment>        
         )
     }
